467575315f5419dc767dc4ef181795f233baa30c
Don Kackman <dkackman@gmail.com>
2020-12-19 13:40:38 -0600

    model complete

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# covid
-Simple analysis and predictions of covid
+# covid model
+
+Simple analysis and predictions of covid using python, jupyter, and sklearn.
+
+## Rationale
+
+The assumption is that the number of deaths today is a function of the number of cases discovered at some point in the past. Using [The COVID Track Project's](https://covidtracking.com/data) data and a simple linear regression, find the best correlation between past cases and deaths.
+
+## Result
+
+### Daily
+
+
+### Smoothed
---
+++ model.py
@@ -0,0 +1,51 @@
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+def FindBestModelOffset(new_cases, new_deaths, max_offset):
+    bestModel = None
+    bestR2 = 0.0
+    bestPredictions = None
+    bestDeaths = None
+    bestOffset = 0
+
+    for offset in range(1, max_offset):
+        cases = new_cases[-0:-offset]
+        deaths = new_deaths[offset:]
+
+        model, r2, predictions = Measure(cases, deaths)
+        if (r2 > bestR2):
+            bestModel = model
+            bestR2 = r2
+            bestPredictions = predictions
+            bestDeaths = deaths
+            bestOffset = offset
+
+    return bestModel, bestR2, bestPredictions, bestDeaths, bestOffset
+
+
+def Measure(cases, deaths):
+    model = LinearRegression()
+    model.fit(cases, deaths)
+    predictions = model.predict(cases)
+    r2 = metrics.r2_score(deaths, predictions)
+
+    return model, r2, predictions
+
+
+def Predict(model, dates, cases, deaths, offset):
+    # create a new date series for the range over which we will predict
+    minDate = np.amin(dates) + np.timedelta64(offset, 'D')
+    maxDate = np.amax(dates) + np.timedelta64(offset + 1, 'D')
+    projected_dates = pd.Series([date for date in np.arange(minDate, maxDate, dt.timedelta(days=1))])
+    projected_deaths = model.predict(cases)
+
+    # pad deaths with enough nan values to make the smae lenght as the projection
+    empty = np.empty(offset)
+    empty[:] = np.nan
+    #deaths_ = deaths.append(pd.Series(empty))
+
+    return projected_dates, deaths, projected_deaths
