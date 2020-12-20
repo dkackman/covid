43d94c0953dc09d38d28810b1b84344303970c9a
Don Kackman <dkackman@gmail.com>
2020-12-19 20:33:28 -0600

    commenting

--- model.py
+++ model.py
@@ -12,11 +12,12 @@ def FindBestModelOffset(new_cases, new_deaths, max_offset):
     bestDeaths = None
     bestOffset = 0
 
+    # loop through the offsets for the one where cases best correlate to future deaths
     for offset in range(1, max_offset):
         cases = new_cases[-0:-offset]
         deaths = new_deaths[offset:]
 
-        model, r2, predictions = Measure(cases, deaths)
+        model, r2, predictions = Model(cases, deaths)
         if (r2 > bestR2):
             bestModel = model
             bestR2 = r2
@@ -27,7 +28,7 @@ def FindBestModelOffset(new_cases, new_deaths, max_offset):
     return bestModel, bestR2, bestPredictions, bestDeaths, bestOffset
 
 
-def Measure(cases, deaths):
+def Model(cases, deaths):
     model = LinearRegression()
     model.fit(cases, deaths)
     predictions = model.predict(cases)
@@ -38,13 +39,13 @@ def Measure(cases, deaths):
 
 def Predict(model, dates, cases, deaths, offset):
     # create a new date series for the range over which we will predict
-    minDate = np.amin(dates) # - np.timedelta64(offset, 'D')
+    minDate = np.amin(dates)
     maxDate = np.amax(dates) + np.timedelta64(offset + 1, 'D')
 
     projected_dates = pd.Series([date for date in np.arange(minDate, maxDate, dt.timedelta(days=1))])
     predicted_deaths = model.predict(cases)
 
-    # pad deaths with enough nan values to make the smae lenght as the projection
+    # pad deaths with enough nan values to make the same length as the projection
     empty = np.empty(offset)
     empty[:] = np.nan
     emptySeries = pd.Series(empty)
