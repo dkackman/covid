fcf44e4fe6654c508da5b7dbb5df6349332947c8
Don Kackman <dkackman@gmail.com>
2020-12-20 19:41:38 -0600

    replace Prediciton namedtuple with DataFrame

--- model.py
+++ model.py
@@ -7,8 +7,7 @@ import datetime as dt
 from collections import namedtuple
 
 
-Model = namedtuple('Model', 'linearModel r2 actuals predictions offset')
-Prediction = namedtuple('Prediction', 'dates actuals predictions')
+Model = namedtuple('Model', 'linearRegression r2 actuals predictions offset')
 
 
 def BestFitModel(new_cases, new_deaths, max_offset) -> Model:
@@ -28,7 +27,7 @@ def BestFitModel(new_cases, new_deaths, max_offset) -> Model:
     return best
 
 
-def Predict(model: Model, dates, cases, deaths) -> Prediction:
+def Predict(model: Model, dates, cases, deaths) -> pd.DataFrame:
     # create a new date series for the range over which we will predict
     # (it is wider than the source date range by [offset]. that is how far in the future we can predict)
     minDate = np.amin(dates)
@@ -38,9 +37,12 @@ def Predict(model: Model, dates, cases, deaths) -> Prediction:
 
     # padding so actuals and predictions can be graphed together within dates
     padding = pd.Series(np.full(model.offset, np.nan))
-    
-    predicted_deaths = model.linearModel.predict(cases)
-    actual_deaths = deaths.append(padding)
-    projected_deaths = padding.append(pd.Series(predicted_deaths))
 
-    return Prediction(projected_dates.values, actual_deaths.values, projected_deaths.values)
+    actual_deaths = deaths.append(padding)
+    projected_deaths = padding.append(pd.Series(model.linearRegression.predict(cases)))
+
+    frame = pd.DataFrame({"dates": projected_dates.values, 
+        "actual": actual_deaths.values, 
+        "projected": projected_deaths.values})
+
+    return frame.melt(id_vars=['dates'], var_name='series', value_name='deaths')        
